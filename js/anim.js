2529cd1f5cc4f7d5efbb290640738ff01b83fa6f
darioamade <dario_isd@outlook.com>
2020-07-22 16:54:27 +0100

    commit
    added new files

--- js/anim.js
+++
@@ -1,6 +0,0 @@
-
-
-gsap.from(".hero-cards-content-left-card2", { duration: 1, x: "-300%", ease: "in", delay: 0 });
-gsap.from(".hero-cards-content-left-card1", { duration: 1, x: "-300%", ease: "in", delay: 1 });
-gsap.from(".stars", { duration: 1, x: "-300%", ease: "in", delay: 1 });
-gsap.from(".hero-cards-img", { duration: 1, y: "-500%", fade: "in", delay: 1 });
